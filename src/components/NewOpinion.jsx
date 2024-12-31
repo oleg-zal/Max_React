b9b19121084e8ca64c4b10018f316bcd9d3734e0
Oleg Zalozhenkov <oleg.zalozhenkov@gmail.com>
2024-12-31 22:41:57 +0100

    09-useformstatus

--- src/components/NewOpinion.jsx
+++ src/components/NewOpinion.jsx
@@ -1,6 +1,7 @@
 import { useActionState, use } from 'react';
 
 import { OpinionsContext } from '../store/opinions-context';
+import Submit from './Submit';
 
 export function NewOpinion() {
   const { addOpinion } = use(OpinionsContext);
@@ -86,9 +87,7 @@ export function NewOpinion() {
           </ul>
         )}
 
-        <p className="actions">
-          <button type="submit">Submit</button>
-        </p>
+        <Submit />
       </form>
     </div>
   );
