edd320626f7c59a263c7e8d82783a4c2b5ec5046
Oleg Zalozhenkov <oleg.zalozhenkov@gmail.com>
2025-01-03 13:23:20 +0100

    11-finished

--- src/components/Events/NewEventsSection.jsx
+++ src/components/Events/NewEventsSection.jsx
@@ -8,7 +8,7 @@ import { fetchEvents } from '../../util/http.js';
 export default function NewEventsSection() {
   const { data, isPending, isError, error } = useQuery({
     queryKey: ['events'],
-    queryFn: fetchEvents,
+    queryFn: ({ signal, queryKey }) => fetchEvents({ signal, ...queryKey[1] }),
     staleTime: 5000,
     // gcTime: 1000
   });
